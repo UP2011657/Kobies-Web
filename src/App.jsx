04960b73dd2e6650e408070a7d37bf3ff63787d9
UP2011657 <91795488+UP2011657@users.noreply.github.com>
2022-05-29 02:38:50 +0100

    Burger menu

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './stying/BurgerStyle.css'
+import Navbar from './components/Navbar.jsx';
+
+import './stying/App.scss';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="App">
+        <p>Home</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+import { slide as Menu } from 'react-burger-menu'
+
+
+
+
+function Navbar(){
+    return(
+        <Menu right>
+            <ul>
+                <li>test 1</li>
+                <li>test 2</li>
+            </ul>
+        </Menu>
+    )
+}
+
+export default Navbar;
