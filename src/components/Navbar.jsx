1436e18bace40d806c5aa42d88c987bb629ce1ea
UP2011657 <91795488+UP2011657@users.noreply.github.com>
2022-05-29 03:01:34 +0100

    Menu Placeholders

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,8 @@ import { push as Menu } from 'react-burger-menu'
 function Navbar(){
     return(
         <Menu right pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-            <ul>
-                <li>test 1</li>
-                <li>test 2</li>
-            </ul>
+            <a>Home</a>
+            <a>Another Page</a>
         </Menu>
     )
 }
