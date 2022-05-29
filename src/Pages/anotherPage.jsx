df0cb4b5b8bf3b2fb0f69edd09697323e1813e8c
UP2011657 <91795488+UP2011657@users.noreply.github.com>
2022-05-29 11:56:47 +0100

    fixed audio not playing on refresh

--- src/Pages/anotherPage.jsx
+++ src/Pages/anotherPage.jsx
@@ -4,14 +4,18 @@ import '../styling/disco.scss';
 import dancer from '../Images/Disco/Dancer.gif'
 import song from '../Audio/song.mp3'
 
-function App() {
+function AudioSetup(){
   const audio = new Audio(song);
   audio.loop = true;
   audio.play();
+}
+
+function App() {
 
   return (
       <div >
         {/*Content*/}
+        {AudioSetup()}
         <div className="Red"></div>
         <div className="Orange"></div>
         <div className="Yellow"></div>
